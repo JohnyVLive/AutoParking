3537fe17d5f96924b4657d56d85b6f7dbecb3079
Evgeniy V. Radchenko <johny.live@koshmart.ru>
2020-12-05 15:19:36 +0300

    Remove Constants file

--- AutoParking/Model/Constants.swift
+++ AutoParking/Model/Constants.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Evgeniy Radchenko on 05.12.2020.
 //
-// 
 
 import Foundation
 
--- AutoParking/Model/SessionModel.swift
+++ AutoParking/Model/SessionModel.swift
@@ -3,7 +3,7 @@
 //  AutoParking
 //
 //  Created by Evgeniy Radchenko on 02.12.2020.
-// Test gitignore 3
+// 
 
 import Foundation
 
